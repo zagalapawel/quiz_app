5606950e3676220d93a4f52b0dbe600c4b020c93
zagalapawel <zagalapawel@gmail.com>
2020-10-13 18:15:45 +0200

    using onPressed with function

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,10 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer chosen!');
+  }
+
   @override
   Widget build(BuildContext context) {
     //var questions = ['What\'s your favorite color?', 'What\'s your favorite animal']
@@ -18,9 +22,9 @@ class MyApp extends StatelessWidget {
         body: Column(
           children: [
             Text('The question!'),
-            RaisedButton(child: Text('Answer 1'), onPressed: null),
-            RaisedButton(child: Text('Answer 2'), onPressed: null),
-            RaisedButton(child: Text('Answer 3'), onPressed: null),
+            RaisedButton(child: Text('Answer 1'), onPressed: answerQuestion),
+            RaisedButton(child: Text('Answer 2'), onPressed: answerQuestion),
+            RaisedButton(child: Text('Answer 3'), onPressed: answerQuestion),
           ],
         ),
       ),
